405514f27681d50260639b454a9115b80f5e4a10
sumtuckyi <sumin510@naver.com>
2023-03-06 07:24:19 +0900

    second

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -4,15 +4,16 @@ import styles from "./nav_bar.module.css";
 function nav () {
     return (
         <div>
-            <nav>
+            <nav className={styles.nav}>
                 <ul className={styles.bar}>
                     <Link to={`${process.env.PUBLIC_URL}/`}><li className={styles.option}>Home</li></Link>
                     <Link to={`/tv_series`}><li className={styles.option}>TV Shows</li></Link>
                     <Link to={`/recent`}><li className={styles.option}>Recent</li></Link>
-                    <input className={styles.input} placeholder="search"></input>
-                    <button className={styles.button}><img src="./1.png"/>go</button>
                 </ul>
-    
+                <div className={styles.search}>
+                    <input className={styles.input} placeholder="search"></input>
+                    <button className={styles.button}>go</button>
+                </div>
             </nav>
         </div>
     );
